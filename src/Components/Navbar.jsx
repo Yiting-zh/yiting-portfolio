1645b2b639cebf521ec6288b53c18c3280b42fca
Yiting-zh <zhangyiting.yiting@outlook.com>
2025-06-10 13:30:01 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => (
   <nav className="navbar">
     <div className="section-container navbar-container">
-      <img src="/Logo.svg" alt="Logo" className="navbar-logo" id="navbar-logo" />
+      <img src={`${import.meta.env.BASE_URL}Logo.svg`} alt="Logo" className="navbar-logo" id="navbar-logo" />
       <div className="navbar-links">
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
